5377c2c0a164fae87e3706577a0796b1ed7b1819
YoungHaChoi <askillofgod159@naver.com>
2024-05-02 17:45:00 +0900

    cation update

--- src/main/java/workoutprj/workout/Controller/CaptionController.java
+++ src/main/java/workoutprj/workout/Controller/CaptionController.java
@@ -4,11 +4,14 @@ import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 import workoutprj.workout.Service.ICaptionService;
 
 @Controller
@@ -21,13 +24,17 @@ public class CaptionController {
 
     @PostMapping("/getCaption")
     @ResponseBody
-    public String getCaption(HttpServletRequest request) {
+    public ResponseEntity<String> getCaption(HttpServletRequest request) {
         String youtubeUrl = request.getParameter("youtubeUrl");
 
         String videoId = youtubeUrl.split("v=")[1].trim();
 
         logger.info("videoId: " + videoId);
-        return captionService.getCaption(videoId).block();
+        return captionService.getCaption(videoId).
+                onErrorMap(ex -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error fetching caption"))
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.status(HttpStatus.NOT_FOUND).body("Caption not found"))
+                .block();
     }
 
     @GetMapping("/subtitle")
