0c1415dccc85a6c3fd8ac1e0ffb23d90619afb9b
YoungHaChoi <askillofgod159@naver.com>
2024-05-13 21:13:58 +0900

    aop, interceptor log to kafka

--- src/main/java/workoutprj/workout/ETC/MonitoringAspect.java
+++ src/main/java/workoutprj/workout/ETC/MonitoringAspect.java
@@ -2,8 +2,8 @@ package workoutprj.workout.ETC;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletRequestAttributeEvent;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -33,10 +33,16 @@ public class MonitoringAspect {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    public MonitoringAspect(){
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
     private String toJson(Object obj) {
         try {
             return objectMapper.writeValueAsString(obj);
         } catch (JsonProcessingException e) {
+            log.info(e.getMessage());
             return "{}";
         }
     }
@@ -48,45 +54,45 @@ public class MonitoringAspect {
     public void databaseTransactionMethod(){}
 
 
-    // 네트워크 요청 처리 전 로그 기록
-    @Before("networkRequestMethods() || databaseTransactionMethod()")
-    public void beforeMethodCall(JoinPoint joinPoint) {
-        HttpServletRequest request = ((ServletRequestAttributes)
-                RequestContextHolder.currentRequestAttributes()).getRequest();
-        Map<String, Object> logMap = new HashMap<>();
-        logMap.put("event", "request_received");
-        logMap.put("method", request.getMethod());
-        logMap.put("uri", request.getRequestURI());
-        logMap.put("remoteAddr", request.getRemoteAddr());
-        logMap.put("timestamp", Instant.now());
-        sendLog(toJson(logMap));
-    }
-
-    // 요청 처리 후 성공적 완료 로그
-    @AfterReturning(pointcut = "networkRequestMethods() || databaseTransactionMethod()", returning = "result")
-    public void afterReturningMethodCall(JoinPoint joinPoint, Object result) {
-        Map<String, Object> logMap = new HashMap<>();
-        logMap.put("event", "request_completed");
-        logMap.put("signature", joinPoint.getSignature().getName());
-        if (result instanceof ResponseEntity) {
-            ResponseEntity<?> response = (ResponseEntity<?>) result;
-            logMap.put("status", response.getStatusCode().value());
-            logMap.put("responseBody", response.getBody());
-        }
-        logMap.put("timestamp", Instant.now());
-        sendLog(toJson(logMap));
-    }
-
-    // 예외 발생 시 로그 기록
-    @AfterThrowing(pointcut = "networkRequestMethods() || databaseTransactionMethod()", throwing = "ex")
-    public void afterThrowingMethodCall(JoinPoint joinPoint, Throwable ex) {
-        Map<String, Object> logMap = new HashMap<>();
-        logMap.put("event", "error");
-        logMap.put("signature", joinPoint.getSignature().getName());
-        logMap.put("error", ex.getMessage());
-        logMap.put("timestamp", Instant.now());
-        sendLog(toJson(logMap));
-    }
+//    // 네트워크 요청 처리 전 로그 기록
+//    @Before("networkRequestMethods()")
+//    public void beforeMethodCall(JoinPoint joinPoint) {
+//        HttpServletRequest request = ((ServletRequestAttributes)
+//                RequestContextHolder.currentRequestAttributes()).getRequest();
+//        Map<String, Object> logMap = new HashMap<>();
+//        logMap.put("event", "request_received");
+//        logMap.put("method", request.getMethod());
+//        logMap.put("uri", request.getRequestURI());
+//        logMap.put("remoteAddr", request.getRemoteAddr());
+//        logMap.put("timestamp", Instant.now());
+//        sendLog(toJson(logMap));
+//    }
+//
+//    // 요청 처리 후 성공적 완료 로그
+//    @AfterReturning(pointcut = "networkRequestMethods()", returning = "result")
+//    public void afterReturningMethodCall(JoinPoint joinPoint, Object result) {
+//        Map<String, Object> logMap = new HashMap<>();
+//        logMap.put("event", "request_completed");
+//        logMap.put("signature", joinPoint.getSignature().getName());
+//        if (result instanceof ResponseEntity) {
+//            ResponseEntity<?> response = (ResponseEntity<?>) result;
+//            logMap.put("status", response.getStatusCode().value());
+//            logMap.put("responseBody", response.getBody());
+//        }
+//        logMap.put("timestamp", Instant.now());
+//        sendLog(toJson(logMap));
+//    }
+//
+//    // 예외 발생 시 로그 기록
+//    @AfterThrowing(pointcut = "networkRequestMethods()", throwing = "ex")
+//    public void afterThrowingMethodCall(JoinPoint joinPoint, Throwable ex) {
+//        Map<String, Object> logMap = new HashMap<>();
+//        logMap.put("event", "request_error");
+//        logMap.put("signature", joinPoint.getSignature().getName());
+//        logMap.put("error", ex.getMessage());
+//        logMap.put("timestamp", Instant.now());
+//        sendLog(toJson(logMap));
+//    }
 
     // DB 트랜잭션 시작 전 로그 기록
     @Before("databaseTransactionMethod()")
@@ -96,6 +102,7 @@ public class MonitoringAspect {
         logMap.put("signature", joinPoint.getSignature().toShortString());
         logMap.put("arguments", Arrays.toString(joinPoint.getArgs()));
         logMap.put("timestamp", Instant.now());
+
         sendLog(toJson(logMap));
     }
 
@@ -107,6 +114,7 @@ public class MonitoringAspect {
         logMap.put("signature", joinPoint.getSignature().toShortString());
         logMap.put("result", result);
         logMap.put("timestamp", Instant.now());
+
         sendLog(toJson(logMap));
     }
 
@@ -118,6 +126,7 @@ public class MonitoringAspect {
         logMap.put("signature", joinPoint.getSignature().toShortString());
         logMap.put("error", ex.getMessage());
         logMap.put("timestamp", Instant.now());
+
         sendLog(toJson(logMap));
     }
 
--- src/main/java/workoutprj/workout/ETC/MonitoringInterceptor.java
+++ src/main/java/workoutprj/workout/ETC/MonitoringInterceptor.java
@@ -1,18 +1,53 @@
 package workoutprj.workout.ETC;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component
 public class MonitoringInterceptor implements HandlerInterceptor {
 
+    @Autowired
+    private WebClient webClient;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public MonitoringInterceptor(){
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    }
+
+    private String toJson(Object obj) {
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            log.info(e.getMessage());
+            return "{}";
+        }
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String uri = request.getRequestURI();
+        // 정적 리소스 URI인지 확인
+        if (uri.matches(".*(css|js|png|jpg|jpeg|gif|woff|woff2|ttf|svg)$") || uri.equals("/error")) {
+            return true; // 정적 리소스에 대해서는 처리를 스킵합니다
+        }
+
         long startTime = System.currentTimeMillis();
         request.setAttribute("startTime", startTime);
         return true;
@@ -25,9 +60,44 @@ public class MonitoringInterceptor implements HandlerInterceptor {
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        long startTime = (Long) request.getAttribute("startTime");
+        Object startTimeObj = request.getAttribute("startTime");
+        if (startTimeObj == null) {
+            return;
+        }
+
+        long startTime = (Long) startTimeObj;
         long endTime = System.currentTimeMillis();
         long executeTime = endTime - startTime;
-        log.info("[" + request.getMethod() + "]" + request.getRequestURI() + " executed in " + executeTime + "ms");
+
+        Map<String, Object> pMap = new HashMap<>();
+        pMap.put("http_method", request.getMethod());
+        pMap.put("http_uri", request.getRequestURI());
+        pMap.put("http_start_time", startTime);
+        pMap.put("http_end_time", endTime);
+        pMap.put("http_execute_time", executeTime + "ms");
+
+        // 로그 포맷 통일
+        String logMessage = String.format("[%s] %s executed in %dms", request.getMethod(), request.getRequestURI(), executeTime);
+        sendLog(toJson(pMap));
+        log.info(logMessage);
+
+        // 예외 발생 시 로깅
+        if (ex != null) {
+            log.error("An exception occurred after handling the request.", ex);
+        }
+    }
+
+
+    private void sendLog(String message) {
+        webClient.post()
+                .uri("/log")
+                .body(Mono.just(message), String.class)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .onErrorResume(e -> {
+                    log.error("Failed to send log to server, error: {}", e.getMessage());
+                    return Mono.empty();
+                })
+                .subscribe();
     }
 }
