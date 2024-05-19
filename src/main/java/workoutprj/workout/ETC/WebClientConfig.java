e4099e84b276cbb6dc4a27c4d0d92641065b3b71
YoungHaChoi <askillofgod159@naver.com>
2024-05-20 02:01:58 +0900

    fix: netty http server request error debug

--- src/main/java/workoutprj/workout/ETC/WebClientConfig.java
+++ src/main/java/workoutprj/workout/ETC/WebClientConfig.java
@@ -1,5 +1,6 @@
 package workoutprj.workout.ETC;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,8 +8,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${ec2.ip}")
+    private String e2_ip;
+
     @Bean
-    public WebClient webClient(){
-        return WebClient.create("http://localhost:8081");
+    public WebClient webClient() {
+        String baseUrl = String.format("http://%s:8081", e2_ip);
+        return WebClient.builder()
+                        .baseUrl(baseUrl)
+                        .build();
     }
 }
